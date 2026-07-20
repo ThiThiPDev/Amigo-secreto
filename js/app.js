ec529fc634043ba2bd83531845187325c823ff62
Thiago Fernandes <tf91883@gmail.com>
2026-07-20 19:19:47 -0300

    Projeto inicial

---
+++ js/app.js
@@ -0,0 +1,67 @@
+let amigos = [];
+function adicionar() {
+
+    let amigo = document.getElementById('nome-amigo');
+    let nome = amigo.value.toUpperCase();
+
+    if(amigo.value == '') {
+        alert('Informe o nome do amigo!');
+        return;
+    }
+
+     if(amigos.includes(nome)) {
+        alert('Nome já adicionado!');
+        return;
+    }
+
+    let lista = document.getElementById('lista-amigos');
+    amigos.push(nome);
+    if(lista.textContent == '') {
+        lista.textContent = nome;
+    } else {
+        lista.textContent = lista.textContent + ', ' + nome;
+    }
+    amigo.value = '';
+}
+
+function sortear() {
+
+    if(amigos.length < 4) {
+        alert('Adicione pelo menos 4 amigos!');
+        return;
+    }
+
+    embaralha(amigos);
+    let sorteio = document.getElementById('lista-sorteio');
+
+    for (let i = 0; i < amigos.length; i++) {
+        if (i == amigos.length - 1) {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[0] + '<br>'
+        } else {
+            sorteio.innerHTML = sorteio.innerHTML + amigos[i] + ' --> ' + amigos[i + 1] + '<br>'
+        }
+    }
+}
+
+function embaralha(lista) {
+
+    for (let indice = lista.length; indice; indice--) {
+
+        const indiceAleatorio = Math.floor(Math.random() * indice);
+
+        // atribuição via destructuring
+        [lista[indice - 1], lista[indiceAleatorio]] = 
+            [lista[indiceAleatorio], lista[indice - 1]];
+    }
+}
+
+function atualizarSorteio() {
+    let sorteio = document.getElementById('lista-sorteio');
+    sorteio.innerHTML = '';
+}
+
+function reiniciar() {
+    amigos = [];
+    document.getElementById('lista-amigos').innerHTML = '';
+    document.getElementById('lista-sorteio').innerHTML = '';
+}
